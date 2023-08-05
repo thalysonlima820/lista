872a00f7cc81c97a2e6cda7db9f0f41341eecbbb
thalysonlima820 <thalysonlima820@gmail.com>
2023-08-05 10:36:50 -0300

    tentativa 1

--- conexao.php
+++ conexao.php
@@ -1,17 +1,13 @@
 <?php
 
-    $host = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-    $banco = 'lista';
+    $MYSQL_URL = 'mysql://root:dXoE7fIBLN5eJlWQHfbr@containers-us-west-109.railway.app:7207/railway';
+    $MYSQLDATABASE = 'railway';
+    $MYSQLHOST = 'containers-us-west-109.railway.app';
+    $MYSQLPASSWORD = 'dXoE7fIBLN5eJlWQHfbr';
+    $MYSQLPORT = '7207';
+    $MYSQLUSER = 'root';
 
-    $conexao = new mysqli ($host, $usuario, $senha, $banco);
-
-
-//    if($conexao->connect_errno)
-//    { echo "Errrro"; }
-//    else
-//    { echo "Conexão efetuada com sucesso"; }
+    $conexao = mysqli_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_PORT);
 
 
 
