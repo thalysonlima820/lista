a25fd4950adad44058bcf5a3c6cc3e3d04fe853c
thalysonlima820 <thalysonlima820@gmail.com>
2023-08-05 10:42:22 -0300

    hfeuwf

--- conexao.php
+++ conexao.php
@@ -1,14 +1,12 @@
 <?php
+$MYSQL_URL = 'mysql://root:dXoE7fIBLN5eJlWQHfbr@containers-us-west-109.railway.app:7207/railway';
+$MYSQLDATABASE = 'railway';
+$MYSQLHOST = 'containers-us-west-109.railway.app';
+$MYSQLPASSWORD = 'dXoE7fIBLN5eJlWQHfbr';
+$MYSQLPORT = '7207';
+$MYSQLUSER = 'root';
 
-    $MYSQL_URL = 'mysql://root:dXoE7fIBLN5eJlWQHfbr@containers-us-west-109.railway.app:7207/railway';
-    $MYSQLDATABASE = 'railway';
-    $MYSQLHOST = 'containers-us-west-109.railway.app';
-    $MYSQLPASSWORD = 'dXoE7fIBLN5eJlWQHfbr';
-    $MYSQLPORT = '7207';
-    $MYSQLUSER = 'root';
-
-    $conexao = mysqli_connect($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_PORT);
-
+$conexao = mysqli_connect($MYSQLHOST, $MYSQLUSER, $MYSQLPASSWORD, $MYSQLDATABASE, $MYSQLPORT);
 
 
 ?>
